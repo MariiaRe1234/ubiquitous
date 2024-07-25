4e1c6ae72b63a95285e2828bde8f4b451e6f79ad
MariiaRe1234 <repinamasha1234@gmail.com>
2024-07-25 13:45:47 +0200

    Add files via upload
    csv to json

---
+++ task_9.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+
+csv_file_path = 'cars.csv'
+json_file_path = 'cars.json'
+
+
+with open(csv_file_path, mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    cars_list = [row for row in csv_reader]
+
+
+with open(json_file_path, mode='w') as json_file:
+    json.dump(cars_list, json_file, indent=2)
+
+# завернуть все в функцию
